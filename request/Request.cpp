3a76c03c74f5d6be379e3e620129ffa7a47c9072
Yevs <yevheniy.yevsyuhov@outlook.com>
2014-07-17 14:09:41 +0300

    Added request files

--- request/Request.cpp
+++ request/Request.cpp
@@ -1,2 +1,51 @@
 #include "request.h"
+#include <QTextStream>
+#include <QStringList>
 
+RequestManager* RequestManager::GetRequestManagerInstance() {
+    if (!(RequestManagerInstance))
+        RequestManagerInstance = new RequestManager();
+    return RequestManagerInstance;
+}
+
+RequestManager::RequestManager() {
+    requestIndex = 0;
+}
+
+void RequestManager::ReadDb() {
+    if (!input.open(QIODevice::ReadOnly | QIODevice::Text))
+             return;
+    QTextStream in(&input);
+    QString line;
+    while (!in.atEnd()) {
+        line = in.readLine();
+        ProcessLine(line);
+    }
+}
+
+void RequestManager::ProcessLine(QString line) {
+    QStringList requestList = line.split(" ", QString::SkipEmptyParts);
+    QString timestampString = requestList.at(0);
+    QString cmdString = requestList.at(1);
+    QString nameString = requestList.at(2);
+    QString parentNameString = "";
+    if (requestList.size() == 4)
+        parentNameString = requestList.at(3);
+    sRequest req = nullRequest;
+    req.timestamp = timestampString.toInt();
+    if (cmdString == "ADD")
+        req.cmd = ADD;
+    else
+        req.cmd = DELETE;
+    req.name = nameString;
+    req.parentName = parentNameString;
+    Requests.push_back(req);
+}
+
+sRequest RequestManager::getNextRequest(ull time) {
+    if (requestIndex >= Requests.size())
+        return sRequest(0, ADD, "", "");
+    if (time >= Requests[requestIndex].timestamp)
+        return sRequest(0, ADD, "", "");
+    return Requests[requestIndex++];
+}
--- request/request.h
+++ request/request.h
@@ -1,10 +1,11 @@
 #ifndef REQUEST_H
 #define REQUEST_H
-#define ull unsigned long long int
+typedef unsigned long long int ull;
 
 #include <vector>
 #include <QString>
 #include <QFile>
+#include <QString>
 
 enum eCommand {
     ADD,
@@ -16,17 +17,33 @@ struct sRequest {
     eCommand cmd;
     QString name;
     QString parentName;
+
+    sRequest(ull _timestamp,
+             eCommand _cmd,
+             QString _name,
+             QString _parentName)
+    {
+        timestamp = _timestamp;
+        cmd = _cmd;
+        name = _name;
+        parentName = _parentName;
+    }
 };
+
+sRequest nullRequest(0, ADD, "", "");
 
 class RequestManager {
 public:
     static RequestManager* GetRequestManagerInstance();
-    const QString FILEPATH = "requests.dat";
+    sRequest getNextRequest(ull time);
 private:
+    int requestIndex;
     RequestManager();
-    RequestManager* RequestManagerInstance;
+    static RequestManager* RequestManagerInstance;
     std::vector <sRequest> Requests;
     QFile input;
+    void ProcessLine(QString line);
+    void ReadDb();
 };
 
 #endif // REQUEST_H
