7d0ef1cffb143cc48df4289581a87f160a7addeb
Markus <markus@afgor.com>
2014-07-17 13:53:19 +0300

    Tree start. Starter methods

--- core/tree.cpp
+++ core/tree.cpp
@@ -1,10 +1,21 @@
 #include "tree.h"
 #include "../request/request.h"
 #include <QVector>
+#include "node.h"
 
 tree::tree()
 {}
 
 void processRequest(sRequest r){
+    int _NodeCenterX;
+    int _NodeCenterY;
+
+    int _LineAx;
+    int _lineAy;
+    int _LineBx;
+    int _LineBy;
+
+    node _Parent;
+
 
 }
--- core/tree.h
+++ core/tree.h
@@ -1,15 +1,25 @@
 #ifndef TREE_H
 #define TREE_H
 #include "../request/request.h"
+#include "node.h"
 #include <QVector>
 
 class tree
 {
 public:
-    tree();
-    void processRequest(sRequest r);
-private:
-    void addNode();
+        tree();
+        ~tree();
+
+        void insert(int key);
+        node *search(int key);
+        void destroy_tree();
+
+    private:
+        void destroy_tree(node *leaf);
+        void insert(int key, node *leaf);
+        node *search(int key, node *leaf);
+
+        node *root;
 };
 
 #endif // TREE_H
