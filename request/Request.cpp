d05f857adc93f2167633b67e67fd84ed1b4c1fcd
Yevs <yevheniy.yevsyuhov@outlook.com>
2014-07-17 13:20:44 +0300

    Added request header

--- request/Request.cpp
+++ request/Request.cpp
@@ -1,5 +1,2 @@
 #include "request.h"
 
-Request::Request()
-{
-}
--- request/request.h
+++ request/request.h
@@ -1,10 +1,32 @@
 #ifndef REQUEST_H
 #define REQUEST_H
+#define ull unsigned long long int
 
-class Request
-{
+#include <vector>
+#include <QString>
+#include <QFile>
+
+enum eCommand {
+    ADD,
+    DELETE
+};
+
+struct sRequest {
+    ull timestamp;
+    eCommand cmd;
+    QString name;
+    QString parentName;
+};
+
+class RequestManager {
 public:
-    Request();
+    static RequestManager* GetRequestManagerInstance();
+    const QString FILEPATH = "requests.dat";
+private:
+    RequestManager();
+    RequestManager* RequestManagerInstance;
+    std::vector <sRequest> Requests;
+    QFile input;
 };
 
 #endif // REQUEST_H
