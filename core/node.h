737ac332ffc932fdbda7b7c22299316634506b8a
Markus <benovskym@gmail.com>
2014-07-21 20:47:07 +0300

    Пофіксати тіки і все

--- core/node.h
+++ core/node.h
@@ -10,11 +10,13 @@ struct node
   node* parent;
   int x, y;
   int Childs;
+  int OFFSET_X;
 
   node(QString _name, node* _parent)
   {
       x = 0;
       y = 0;
+      OFFSET_X = 0;
       Childs = 0;
       name = _name;
       parent = _parent;
