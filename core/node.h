c689e9705282161c098fb9f27ba75ddb5eca7511
Yevs <yevheniy.yevsyuhov@outlook.com>
2014-07-23 13:34:30 +0300

    Modificated node

--- core/node.h
+++ core/node.h
@@ -2,6 +2,7 @@
 #define NODE_H
 #include <QString>
 #include <vector>
+#include "../opengl/RenderTypes.h"
 
 struct node
 {
@@ -9,15 +10,36 @@ struct node
   uint texture;
   std::vector <node*> nodes;
   node* parent;
+  Vertex2F oldState;
+  Vertex2F newState;
   float x, y;
+  float passedTime;
+  float fullTime;
 
   node(QString _name, node* _parent, uint _texture)
   {
+      fullTime = 1000;
+      passedTime = 0;
       x = 0;
       y = 0;
       name = _name;
       parent = _parent;
       texture = _texture;
+  }
+
+  Vertex2F getCoordinates()
+  {
+      if (passedTime <= fullTime)
+      {
+          float xVector, yVector;
+          xVector = newState.x - oldState.x;
+          yVector = newState.y - oldState.y;
+          return {
+              newState.x + (passedTime/fullTime)*xVector,
+              newState.y + (passedTime/fullTime)*yVector
+          };
+      }
+      return newState;
   }
 };
 
